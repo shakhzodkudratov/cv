fcbc36709d70ff5cd97269ea4ee051ed9d9d52b7
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2025-05-02 17:14:02 +0500

    first version

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "cv";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    with flake-utils.lib; eachSystem allSystems (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive) scheme-full latex-bin latexmk;
+      };
+    in rec {
+      packages = {
+        document = pkgs.stdenvNoCC.mkDerivation rec {
+          name = "cv";
+          src = self;
+          buildInputs = [ pkgs.coreutils tex ];
+          phases = ["unpackPhase" "buildPhase" "installPhase"];
+          buildPhase = ''
+            export PATH="${pkgs.lib.makeBinPath buildInputs}";
+            mkdir -p .cache/texmf-var
+            env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+              latexmk -interaction=nonstopmode -pdf -lualatex \
+              cv.tex
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp cv.pdf $out
+          '';
+        };
+      };
+      defaultPackage = packages.document;
+    });
+}
